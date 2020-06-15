4aac12ca012a0ce045376e63283e7ef7b1173329
Hwanghyeonjeong <hyeonjeonghwang10@gmail.com>
2020-06-15 17:09:25 +0900

    모바일 좌측 토글 시작

--- part/head.php
+++ part/head.php
@@ -1,29 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>블로그</title>
-	<link rel="stylesheet" href="/resource/common.css">
-	<script src="/resource/common.js"></script>
-
-    <div class="top-bar height-100p">
-        <div class="menu-box-1 height-100p con">
-            <ul class="flex height-100p">
-                <li class="flex-1-0-0"><a href="/" class="height-100p flex flex-ai-c flex-jc-c">Home</a></li>
-                <li class="flex-1-0-0"><a href="/list.php" class="height-100p flex flex-ai-c flex-jc-c">Articles</a>
-                </li>
-                <li class="flex-1-0-0"><a href="/about.php" class="height-100p flex flex-ai-c flex-jc-c">About Me</a>
-                </li>
-                <li class="flex-1-0-0">
-                    <a href="#" class="height-100p flex flex-ai-c flex-jc-c">SNS</a>
-                    <ul>
-                        <li><a href="#">Facebook</a></li>
-                        <li><a href="#">Instagram</a></li>
-                        <li><a href="#">Github</a></li>
-                    </ul>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>블로그</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+    <link rel="stylesheet" href="/resource/common.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/resource/common.js"></script>
+</head>
+<div class="side-bar">
+    <div class="menu-box-1">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Article</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">SNS</a>
+        <ul>
+            <li><a href="#">FACEBOOK</a></li>
+            <li><a href="#">INSTAGRAM</a></li>
+            <li><a href="#">GITHUB</a></li>
+        </ul></li>
+        </ul>
+    </div>
+</div>
+<body>
+<div class="mobile-top-bar visible-sm-down flex">
+        <a href="#" class="btn-toggle-mobile-side-bar flex-as-c">
+            <div></div>
+            <div></div>
+            <div></div>
+        </a>
+    </div>
+    <div class="top-bar visible-md-up">
+        <div class="con height-100p flex">
+            <a href="/" class="logo flex flex-ai-c">
+                LOGO
+            </a>
+            <nav class="menu-box-1 flex flex-1-0-0">
+                <ul class="flex flex-1-0-0">
+                    <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Home</a></li>
+                    <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Articles</a>
                     </li>
-            </ul>
+                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">About Me</a>
+                    </li>
+                    <li class="flex-1-0-0 flex">
+                        <a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a>
+                        <ul>
+                            
+                            <li>
+                                <a href="https://ko-kr.facebook.com/" target="_blank">
+                                    FACEBOOK
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/?hl=ko" target="_blank">
+                                    INSTAGRAM
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com" target="_blank">
+                                    GITHUB
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
