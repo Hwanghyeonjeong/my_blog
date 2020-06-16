44cd813447a6f8d8474521c95ce6bd053ce4699a
hwanghyeonjeong <hyeonjeonghwang10@gmail.com>
2020-06-16 18:07:50 +0900

    클릭시 사이드바 2차메뉴 나오기

--- part/head.php
+++ part/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,33 +10,47 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/resource/common.js"></script>
 </head>
-<div class="mobile-side-bar-bg">
-    </div>
-<div class="mobile-side-bar">
+<div class="mobile-side-bar-bg visible-sm-down">
+</div>
+<div class="mobile-side-bar visible-sm-down">
     <div class="menu-box-1">
         <ul>
             <li><a href="#">Home</a></li>
-            <li><a href="#">Article</a></li>
+            <li>
+                <a href="#">Articles</a>
+                <ul>
+                    <li><a href="#">Notice</a></li>
+                    <li><a href="#">Fee</a></li>
+                </ul>
+            </li>
             <li><a href="#">About</a></li>
             <li><a href="#">SNS</a>
-        <ul>
-            <li><a href="#">FACEBOOK</a></li>
-            <li><a href="#">INSTAGRAM</a></li>
-            <li><a href="#">GITHUB</a></li>
-        </ul></li>
+                <ul>
+                    <li><a href="https://ko-kr.facebook.com/">FACEBOOK</a></li>
+                    <li><a href="#">INSTAGRAM</a>
+                        <ul>
+                            <li><a href="#">Private</a></li>
+                            <li><a href="#">Public</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="https://github.com">GITHUB</a></li>
+                    <li><a href="https://www.youtube.com/?gl=KR&hl=ko">YOUTUBE</a></li>
+                </ul>
+            </li>
         </ul>
     </div>
 </div>
+
 <body>
-<div class="mobile-top-bar visible-sm-down flex">
-<a href="#" onclick="return false;" class="btn-toggle-mobile-side-bar flex-as-c">
+    <div class="mobile-top-bar visible-sm-down flex">
+        <a href="#" onclick="return false;" class="btn-toggle-mobile-side-bar flex-as-c">
             <div></div>
             <div></div>
             <div></div>
         </a>
     </div>
 
-  
+
 
     <div class="top-bar visible-md-up">
         <div class="con height-100p flex">
@@ -45,14 +60,16 @@
             <nav class="menu-box-1 flex flex-1-0-0">
                 <ul class="flex flex-1-0-0">
                     <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Home</a></li>
-                    <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Articles</a>
+                    <li class="flex-1-0-0 flex"><a href="/list.php"
+                            class="flex flex-1-0-0 flex-ai-c flex-jc-c">Articles</a>
                     </li>
-                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">About Me</a>
+                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">About
+                            Me</a>
                     </li>
                     <li class="flex-1-0-0 flex">
                         <a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a>
                         <ul>
-                            
+
                             <li>
                                 <a href="https://ko-kr.facebook.com/" target="_blank">
                                     FACEBOOK
@@ -61,6 +78,11 @@
                             <li>
                                 <a href="https://www.instagram.com/?hl=ko" target="_blank">
                                     INSTAGRAM
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/?gl=KR&hl=ko" target="_blank">
+                                    YOUTUBE
                                 </a>
                             </li>
                             <li>
