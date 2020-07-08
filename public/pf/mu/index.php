0201952d095ef54386db226734ff861f92549e54
hwanghyeonjeong <hyeonjeonghwang10@gmail.com>
2020-07-08 18:42:18 +0900

    포트폴리오

---
+++ public/pf/mu/index.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>블로그</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+    <link rel="stylesheet" href="./index.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="./index.js"></script>
+
+</head>
+
+<div class="top-bar con flex flex-ai-c">
+    <div class="logo"><div class="img-box"><img src="https://hwanghyeonjeong.github.io/img1/pf/mu/logo.svg" alt=""></div></div>
+    <div class="menu-box-1 flex-1-0-0 height-100p">
+        <ul class="flex flex-jc-c height-100p">
+            <li class="flex-ai-c"><a href="#">이용안내</a> 
+            <ul class="flex-jc-c">
+                <li><a href="#">관람안내</a></li>
+                <li><a href="#">단체관람</a></li>
+                <li><a href="#">층별안내</a></li>
+                <li><a href="#">시설안내</a></li>
+                <li><a href="#">대관안내</a></li>
+                <li><a href="#">기증안내</a></li>
+                <li><a href="#">오시는 길</a></li>
+            </ul> 
+        </li>
+            <li class="flex-ai-c"><a href="#">전시</a>
+            <ul>
+                <li><a href="#">전시</a></li>
+                <li><a href="#">아카이브</a></li>
+            </ul>
+        </li>
+            <li class="flex-ai-c"><a href="#">교육</a>
+            <ul>
+                <li><a href="#">전체보기</a></li>
+                <li><a href="#">어린이</a></li>
+                <li><a href="#">청소년</a></li>
+                <li><a href="#">성인</a></li>
+                <li><a href="#">사회복지</a></li>
+                <li><a href="#">동호회</a></li>
+                <li><a href="#">문화행사</a></li>
+            </ul>
+        </li>
+            <li class="flex-ai-c"><a href="#">소식</a>
+            <ul>
+                <li><a href="#">공고·소식</a></li>
+                <li><a href="#">박물관 새소식</a></li>
+                <li><a href="#">뉴스레터</a></li>
+            </ul>
+        </li>
+            <li class="flex-ai-c"><a href="#">유물</a>
+            <ul>
+                <li><a href="#">소장유물</a></li>
+                <li><a href="#">기증자 현황</a></li>
+                <li><a href="#">유물열람 안내</a></li>
+            </ul>
+        </li>
+            <li class="flex-ai-c"><a href="#">박물관</a>
+            <ul>
+                <li><a href="#">박물관 소개</a></li>
+                <li><a href="#">뮤지엄파크 소개</a></li>
+                <li><a href="#">미션과 비전</a></li>
+                <li><a href="#">연혁</a></li>
+                <li><a href="#">박물관직원</a></li>
+                <li><a href="#">청소년자원봉사</a></li>
+            </ul>
+        </li>
+        </ul>
+    </div>
+    <div class="right-menu">
+        <ul class="flex">
+            <li>
+                <a href="#">지지씨멤버스</a>
+                <i class="fas fa-external-link-alt"></i>
+            </li>
+            <li><a href="#">Q&A</a></li>
+            <li><a href="#">자료실</a></li>
+        </ul>
+    </div>
+    <div class="ham flex-jc-c flex-ai-c"><i class="fas fa-bars"></i></div>
+</div>
+
+<div class="banner-box con">
+    <div class="img-box"><div class="cover">
+        <h2>
+        <p class="main_title">2020년 8월 4일<br>재개관 D - 27일</p>
+        <p class="sub_title">"힘찬 새출발, 여기가 경기!"<br>경기도박물관이 여러분의 관심으로 다시 태어납니다.</p>
+      </h2>
+    </div>
+    <div class="banner-box-2 con flex height-100p">
+        <div class="box box-1 height-100p flex-ai-c"><a href="#"><i class="fas fa-clock"></i>오늘의 관람시간</a></div>
+        <div class="box box-2 height-100p flex-1-0-0 flex-ai-c"> 
+            <a href="#"> 오늘은 휴관입니다.</a>
+              </div>
+        <div class="box box-3 height-100p flex-ai-c"><a href="#"><i class="fas fa-car"></i>오시는 길 바로가기</a></div>
+            
+        </div>
+    </div>
+</div>
+    
