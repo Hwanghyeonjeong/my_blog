2ad390dceb22fcbf529a785fb1358629772d90be
hwanghyeonjeong <hyeonjeonghwang10@gmail.com>
2020-07-15 18:44:41 +0900

    펍지 중간

--- public/pf/pcs/index.js
+++ public/pf/pcs/index.js
@@ -1,40 +1,13 @@
-function dotSlide() {
-    var $this = $(this);
-    var $slider = $this.closest('.slider');
-    var $dotActived = $slider.find('.dots > ul > li.active');
-    var $slideActived = $slider.find('.slides > div.active');
-    var $dotIndex = $this.index();
-    var $slides = $slider.find('.slides > div');
-    var $slideIndex = $slides.eq($dotIndex);
 
-    $dotActived.removeClass('active');
-    $slideActived.removeClass('active');
-    $slideIndex.addClass('active');
-    $this.addClass('active');
-}
-
-function dotSlide__init(){
-    $('.dots > ul > li').click(dotSlide);
-}
-
-function dotsClick(){
-    var $actived = $('.dots > ul > li.active');
-    var $post = $actived.next();
-
-    if ( $post.length == 0 ) {
-        $('.dots > ul > li:first-child').click();
-    }
-    else {
-        $post.click();
-    }
-}
-
-function slideInterval(){
-    setInterval(dotsClick, 3000);
-}
-
-
-$(function() {
-    dotSlide__init();
-    slideInterval();
-});
+$(function(){
+    $('.my-slider-1 > .owl-carousel').owlCarousel({
+        items:1,
+        nav: true,
+        navText: [
+          '<img style="transform: rotate(-180deg);" src="https://hwanghyeonjeong.github.io/img1/pf/pcs/ico_slick_button.png" alt="">',
+          '<img src="https://hwanghyeonjeong.github.io/img1/pf/pcs/ico_slick_button.png" alt="">'
+        ],
+        navContainer: '.my-slider-1 > .custom-nav',
+        loop:true
+      });
+})
--- public/pf/pcs/index.php
+++ public/pf/pcs/index.php
@@ -22,49 +22,176 @@
 <body>
 
 
-<div class="slider con">
-
-        <div class="slides">
-            <div class="active"
-                style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200526_main_PC_01-1500x797.png);">
+    <div class="my-slider-1">
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="background">
+                    <div
+                        style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200526_main_PC_01-1500x797.png);">
+                    </div>
+                    <div
+                        style="background-image:url(https://globalesports.wpengine.com/wp-content/uploads/2020/06/200608_main_Mobile_NA-2.png);">
+                    </div>
+                </div>
+                <div class="txt-box">
+                    <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                    <p class="area">PUBG Esports Player Stats+(PEPS+)</p>
+                    <a class="more" href="https://pubgesports.eu/en">VIEW MORE</a>
+                </div>
             </div>
-
-            <div style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200608_main_PC_APAC-2-1500x797.png);">
+            <div class="item">
+                <div class="background">
+                    <div
+                        style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200608_main_PC_APAC-2-1500x797.png);">
+                    </div>
+                    <div
+                        style="background-image:url(https://globalesports.wpengine.com/wp-content/uploads/2020/06/200608_main_Mobile_NA-2.png);">
+                    </div>
+                </div>
+                <div class="txt-box">
+                    <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                    <p class="area">EUROPE</p>
+                    <a class="more" href="https://pubgesports.eu/en">VIEW MORE</a>
+                </div>
             </div>
-
-            <div style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200608_main_PC_ASIA-2-1500x797.png);">
+            <div class="item">
+                <div class="background">
+                    <div
+                        style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200608_main_PC_ASIA-2-1500x797.png);">
+                    </div>
+                    <div
+                        style="background-image:url(https://globalesports.wpengine.com/wp-content/uploads/2020/06/200608_main_Mobile_NA-2.png);">
+                    </div>
+                </div>
+                <div class="txt-box">
+                    <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                    <p class="area">NORTH AMERICA</p>
+                    <a class="more" href="https://pubgesports.eu/en">VIEW MORE</a>
+                </div>
             </div>
-            <div style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200608_main_PC_EU-2-1500x797.png);">
+            <div class="item">
+                <div class="background">
+                    <div
+                        style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200608_main_PC_EU-2-1500x797.png);">
+                    </div>
+                    <div
+                        style="background-image:url(https://globalesports.wpengine.com/wp-content/uploads/2020/06/200608_main_Mobile_NA-2.png);">
+                    </div>
+                </div>
+                <div class="txt-box">
+                    <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                    <p class="area">ASIA</p>
+                    <a class="more" href="https://pubgesports.eu/en">VIEW MORE</a>
+                </div>
             </div>
-            <div style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200608_main_PC_NA-2-1500x797.png);">
+            <div class="item">
+                <div class="background">
+                    <div
+                        style="background-image:url(https://hwanghyeonjeong.github.io/img1/pf/pcs/200608_main_PC_NA-2-1500x797.png);">
+                    </div>
+                    <div
+                        style="background-image:url(https://globalesports.wpengine.com/wp-content/uploads/2020/06/200608_main_Mobile_NA-2.png);">
+                    </div>
+                </div>
+                <div class="txt-box">
+                    <h2 class="title">PUBG CONTINENTAL SERIES 1</h2>
+                    <p class="area">APAC</p>
+                    <a class="more" href="https://pubgesports.eu/en">VIEW MORE</a>
+                </div>
             </div>
         </div>
-
-        <div class="dots">
-            <ul class="flex">
-                <li class="active"></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
-
+        <div class="custom-nav"></div>
     </div>
 
 
+    <div class="top-bar flex con flex-height-100p">
+        <div class="logo flex-ai-c"><a href="#"><img
+                    src="https://hwanghyeonjeong.github.io/img1/pf/pcs/global-pcs-logo.png" alt=""></a></div>
+        <div class="menu-box-1  flex-grow-1 flex-jc-c flex-1-0-0 flex-ai-c ">
+            <ul class="flex height-100p flex-grow-1  flex-ai-c">
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">TEAMS</a></li>
+                <li><a href="#">SCHEDULE</a></li>
+                <li><a href="#">RANKING</a></li>
+            </ul>
+        </div>
+        <div class="cont flex-ai-c">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 31.951 31.951">
+                <path id="Color_Overlay" data-name="Color Overlay"
+                    d="M721.4,269.975a15.992,15.992,0,0,1-15.956-15.287h-.017v-1.375h.017A15.974,15.974,0,1,1,721.4,269.975h0Zm.689-1.444c2.063-.4,3.87-2.609,5.041-5.838a26.977,26.977,0,0,0-5.041-.585Zm-6.416-5.838c1.171,3.229,2.976,5.433,5.04,5.838v-6.423A26.949,26.949,0,0,0,715.672,262.692Zm12.007,2.235a11.979,11.979,0,0,1-2.061,3.051,14.591,14.591,0,0,0,6.134-3.685,17.24,17.24,0,0,0-3.289-1.272A18.183,18.183,0,0,1,727.679,264.927Zm-16.63-.634a14.583,14.583,0,0,0,6.133,3.685,11.954,11.954,0,0,1-2.06-3.051,18.385,18.385,0,0,1-.785-1.905A17.23,17.23,0,0,0,711.049,264.293Zm17.838-2.6a17.791,17.791,0,0,1,3.82,1.539,14.529,14.529,0,0,0,3.274-8.546h-6.092A28.047,28.047,0,0,1,728.887,261.694Zm-18.795,1.538a17.736,17.736,0,0,1,3.821-1.538,28.089,28.089,0,0,1-1-7.007h-6.092A14.533,14.533,0,0,0,710.092,263.232Zm12-2.5a28.525,28.525,0,0,1,5.468.629,26.692,26.692,0,0,0,.965-6.673h-6.433Zm-6.844.629a28.445,28.445,0,0,1,5.468-.629v-6.043h-6.434A26.709,26.709,0,0,0,715.244,261.36Zm13.644-15.053a28.081,28.081,0,0,1,1,7h6.092a14.538,14.538,0,0,0-3.272-8.544A17.728,17.728,0,0,1,728.888,246.308Zm-6.8.962v6.043h6.433a26.719,26.719,0,0,0-.965-6.674A28.449,28.449,0,0,1,722.088,247.27Zm-7.81,6.043h6.434V247.27a28.531,28.531,0,0,1-5.468-.631A26.71,26.71,0,0,0,714.278,253.312Zm-7.459,0h6.092a28.124,28.124,0,0,1,1-7,17.751,17.751,0,0,1-3.823-1.535A14.531,14.531,0,0,0,706.819,253.312Zm15.269-7.419a26.956,26.956,0,0,0,5.04-.587c-1.171-3.229-2.977-5.432-5.04-5.837Zm-6.416-.587a27.022,27.022,0,0,0,5.04.587V239.47C718.648,239.874,716.843,242.077,715.672,245.307Zm12.007-2.234a18.512,18.512,0,0,1,.783,1.9,17.193,17.193,0,0,0,3.29-1.269,14.593,14.593,0,0,0-6.134-3.686A11.979,11.979,0,0,1,727.679,243.073Zm-16.631.635a17.29,17.29,0,0,0,3.289,1.269,18.639,18.639,0,0,1,.785-1.9,11.954,11.954,0,0,1,2.06-3.051A14.593,14.593,0,0,0,711.048,243.707Z"
+                    transform="translate(-705.424 -238.025)" fill="#fff"></path>
+            </svg>
+        </div>
+    </div>
 
-
-
-
-
-
-
-
-
-
-
-
+    <div class="news">
+        <div class="top flex flex-jc-c">
+            <div class="tit">FEATURED NEWS</div>
+            <div class="all">View All</div>
+        </div>
+        <div class="center">
+            <ul class="flex flex-jc-c">
+                <li>
+                    <a href="#">
+                        <div class="pho-wrap">
+                            <div class="pho"></div>
+                        </div>
+                        <div class="post">
+                            <h3>General/Pcs1</h3>
+                            <h2>Four PUBG Continental Series 1<br> Champions Emerge: Qconfirm, TSM,<br> Infantry, and STK</h2>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="pho-wrap">
+                            <div class="pho"></div>
+                        </div>
+                        <div class="post">
+                            <h3>General</h3>
+                            <h2>Dev Letter: Deep Dive into PUBG<br> Esports Player Stats+(PEPS+)</h2>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="pho-wrap">
+                            <div class="pho"></div>
+                        </div>
+                        <div class="post">
+                            <h3>General/Pcs1</h3>
+                            <h2>PCS1 Match Days 1 &amp; 2: Qconfirm Leads<br> in APAC, STK in North America</h2>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+<div class="banner flex flex-jc-c">
+    <div class="box-1 box">
+        <img src="https://hwanghyeonjeong.github.io/img1/pf/pcs/sample_main_banner01.jpg" alt="">
+        <div class="post">
+            <h3>2020 PCS<br>STREAMING CHANNELS</h3>
+            <div class="sum">Check out English broadcasts for all regions!</div>
+            <ul class="flex-jc-sb flex">
+                <li> <a href="#"> Twitch</a></li>
+                <li><a href="#"> YouTube(PUBG)</a></li>
+                <li><a href="#"> YouTube(Esports)</a></li>
+                <li><a href="#"> More Languages</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="box-2 box">
+        <img src="https://hwanghyeonjeong.github.io/img1/pf/pcs/sample_main_banner02.jpg" alt="">
+        <div class="post">
+            <h3>2020 PUBG<br>GLOBAL ESPORTS RULESET</h3>
+            <div class="sum">Standard & Universal</div>
+            <ul class="flex-jc-sb flex">
+                <li> <a href="#">DOWNLOAD</a></li>
+            </ul>
+        </div>
+</div>  
+</div>
 
 
 
