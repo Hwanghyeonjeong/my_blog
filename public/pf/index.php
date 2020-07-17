53ce41a36034a57312addd31f6484cc3054e62cd
hwanghyeonjeong <hyeonjeonghwang10@gmail.com>
2020-07-17 18:43:46 +0900

    포트폴리오 영역

---
+++ public/pf/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>코딩 작업물</h1>
+    <nav>
+    <ul>
+        <li><a href="https://site9.blog.oa.gg/pf/mu/">경기도 박물관</a></li>
+        <li><a href="http://musenet.ggcf.kr/">원본</a></li>
+
+        <li><a href="https://site9.blog.oa.gg/pf/pcs/">PUBG</a></li>
+        <li><a href="https://www.pubgesports.com/">원본</a></li>
+    </ul>
+    </nav>
+
+
+
+
+
+    <h1>디자인</h1>
+    <nav>
+    <ul>
+        <li><a href="https://hwanghyeonjeong.github.io/img1/pf/artworks/1/1.jpg">인구감소</a></li>
+        <li>창작여부 : 모작</li>
+        <li>출처 : 망고보드</li>
+    </ul>
+    </nav>
+</body>
+</html>
