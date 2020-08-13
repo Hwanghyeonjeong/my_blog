1f7671a96b2b06e23257068838f9623747c46266
Hwanghyeonjeong <hyeonjeonghwang10@gmail.com>
2020-08-13 18:41:16 +0900

    아쿠아리움 program

--- public/pf/aquarium/index.js
+++ public/pf/aquarium/index.js
@@ -1,6 +1,6 @@
 $(function() {
     new fullpage('#fullpage', {
-        sectionsColor: ['rgba(0, 0, 0, 1)', 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, .3)', '#ffffff', '#ffffff', '#ffffff'],
+        sectionsColor: ['rgba(0, 0, 0, 1)', 'rgba(0, 0, 0, .3)', 'rgba(0, 0, 0, .3)', '#ffffff', '#ffffff', '#ffffff'],
       });
     })
     
--- public/pf/aquarium/index.php
+++ public/pf/aquarium/index.php
@@ -25,6 +25,59 @@
 <body>
 
 
+    <div class="top-bar flex">
+        <div class="logo flex-ai-c">
+            <img src="https://hwanghyeonjeong.github.io/img1/pf/aquarium/4.png" alt="">
+        </div>
+        <ul class="menu flex flex-1-0-0 flex-jc-c flex-ai-c">
+            <li><a href="#" class="flex-ai-c">대전아쿠아리움</a>
+                <ul>
+                    <li><a href="#">소개</a></li>
+                    <li><a href="#">찾아오는 길</a>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="#" class="flex-ai-c">테마존</a>
+                <ul>
+                    <li><a href="#">아쿠아리움</a></li>
+                    <li><a href="#">체험동물원</a></li>
+                    <li><a href="#">부대시설</a></li>
+                </ul>
+            </li>
+            <li><a href="#" class="flex-ai-c">체험안내</a></li>
+
+            <li><a href="#" class="flex-ai-c">이용안내</a>
+                <ul>
+                    <li><a href="#">이용안내</a></li>
+                    <li><a href="#">단체관람안내</a></a></li>
+                    <li><a href="#">자주하는 질문</a></li>
+                </ul>
+            </li>
+            <li><a href="#" class="flex-ai-c">고객센터</a>
+                <ul>
+                    <li><a href="#">공지사항</a></li>
+                    <li><a href="#">자주 묻는 질문</a></a></li>
+                    <li><a href="#">고객소리함</a></li>
+                </ul>
+            </li>
+        </ul>
+
+
+
+        <ul class="login flex flex-ai-c">
+            <li class="flex flex-ai-c"><a href="#" class="flex flex-ai-c">
+                    <img class="user"
+                        src="https://hwanghyeonjeong.github.io/img1/pf/aquarium/%EC%BA%A1%EC%B2%98.PNG?dummy" alt="">
+                    <div class="flex-ai-c">Log In</div>
+                    <img class="v" src="https://hwanghyeonjeong.github.io/img1/pf/aquarium/Untitled-2.png" alt="">
+                </a></li>
+            <li class="flex flex-ai-c"><a href="#" class="flex">
+                    <img class="ticket flex-ai-c" src="https://hwanghyeonjeong.github.io/img1/pf/aquarium/ticket.png"
+                        alt="">
+                    <div class="flex-ai-c">티켓구매</div>
+                </a></li>
+        </ul>
+    </div>
 
 
 
@@ -34,72 +87,130 @@
                 <video autoplay muted src="https://hwanghyeonjeong.github.io/img1/pf/aquarium/mov2.mp4?dummy=1234"
                     loop="10"></video>
             </div>
-            <div class="top-bar flex">
-                <div class="logo flex-ai-c">
-                    <img src="https://hwanghyeonjeong.github.io/img1/pf/aquarium/4.png" alt="">
-                </div>
-                <ul class="menu flex flex-1-0-0 flex-jc-c flex-ai-c">
-                    <li><a href="#" class="flex-ai-c">대전아쿠아리움</a>
-                        <ul>
-                            <li><a href="#">소개</a></li>
-                            <li><a href="#">찾아오는 길</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="#" class="flex-ai-c">테마존</a>
-                        <ul>
-                            <li><a href="#">아쿠아리움</a></li>
-                            <li><a href="#">체험동물원</a></li>
-                            <li><a href="#">부대시설</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#" class="flex-ai-c">체험안내</a></li>
 
-                    <li><a href="#" class="flex-ai-c">이용안내</a>
-                        <ul>
-                            <li><a href="#">이용안내</a></li>
-                            <li><a href="#">단체관람안내</a></a></li>
-                            <li><a href="#">자주하는 질문</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#" class="flex-ai-c">고객센터</a>
-                        <ul>
-                            <li><a href="#">공지사항</a></li>
-                            <li><a href="#">자주 묻는 질문</a></a></li>
-                            <li><a href="#">고객소리함</a></li>
-                        </ul>
-                    </li>
-</ul>
-
-
-
-                    <ul class="login flex flex-ai-c">
-                        <li class="flex flex-ai-c"><a href="#" class="flex flex-ai-c">
-                                <img class="user"
-                                    src="https://hwanghyeonjeong.github.io/img1/pf/aquarium/%EC%BA%A1%EC%B2%98.PNG?dummy"
-                                    alt="">
-                                <div class="flex-ai-c">Log In</div>
-                                <img class="v" src="https://hwanghyeonjeong.github.io/img1/pf/aquarium/Untitled-2.png"
-                                    alt="">
-                            </a></li>
-                        <li class="flex flex-ai-c"><a href="#" class="flex">
-                                <img class="ticket flex-ai-c"
-                                    src="https://hwanghyeonjeong.github.io/img1/pf/aquarium/ticket.png" alt="">
-                                <div class="flex-ai-c">티켓구매</div>
-                            </a></li>
-                    </ul>
+            <div class="txt">
+                <p>만지는 즐거움, 느끼는 생생함!<br>우리가족 즐거운 놀이터</p>
+                <p>대전아쿠아리움</p>
             </div>
 
         </div>
+
+
+
+
+
+
+
+
         <div class="section section2">
             <div class="bg">
                 <div class="img-box">
-                
+                    <div class="black"></div>
+                </div>
+            </div>
+            <div class="con">
+                <div class="title">Program</div>
+                <div class="cover">
+                    <div class="pro pro1 flex">
+                        <div class="box box-1">
+                            <div class="tit">다이버의 수중탐사시간</div>
+                            <p>눈앞에서 펼쳐지는 다이버의 즐거운<br>물고기 피딩시간!</p>
+                            <div class="time">평일 탐사시간 11:00, 14:30<br>주말 탐사시간 11:00, 12:30,<br>14:00, 15:30, 17:00</div>
+                        </div>
+                        <div class="box box-2">
+                            <div class="tit">터치풀</div>
+                            <p>살아있는 물고기와 샌드피쉬를 직접 만져볼<br>수 있는 터치풀</p>
+                            <div class="time">물고기 체험관 / 샌드피쉬 체험관</div>
+                        </div>
+                        <div class="box box-3">
+                            <div class="tit">비단잉어 먹이체험</div>
+                            <p>내 손으로 직접 비단잉어에게 먹이를 줄 수 있는<br>비단잉어 먹이체험</p>
+                            <div class="time">먹이요금 1캡슐 당 1,000원<br>(자판기 운영 / 500x2)</div>
+                        </div>
+                        <div class="box box-4">
+                            <div class="text">매주 월요일은<br>악어쇼, 다이버의 수중탐사, 닥터피쉬<br>체험은 운영하지 않습니다.<br>
+                                이용에 참고하여 주시기 바랍니다</div>
+                        </div>
+                    </div>
+                    <div class="pro pro2 flex">
+                        <div class="box box-5">
+                            <div class="tit">닥터피쉬 체험관</div>
+                            <p>피부 각질을 간질간질 떼어먹는 닥터피쉬 체험관!</p>
+                            <div class="time">발만 체험 가능합니다. (손은X)<br>미끄러울 수 있으니, 주의하여 주세요!<br>체험시간 및 요금 10분 2,000원</div>
+                        </div>
+                        <div class="box box-6">
+                            <div class="tit">악어쇼 관람관</div>
+                            <p>태국 정통 조련사들과 악어들이 펼치는 숨막히는<br>대결투로 여러분을 초대합니다</p>
+                            <div class="time">평일 관람시간 12:00, 14:00, 16:00<br>주말 관람시간 11:30, 13:00,<br>14:30, 16:00,
+                                17:30</div>
+                            <div class="price">관람요금 6,000원 (연간회원 4,000원)</div>
+                        </div>
+                        <div class="box box-7">
+                            <div class="tit">체험동물원 먹이주기 체험</div>
+                            <p>20여종의 동물들을 보고, 만지고, 먹이까지<br>줄 수 있는 체험동물원!</p>
+                            <div class="time">체험 요금 실내 당근먹이 1,000원<br>실외 맹수 먹이 1,000원</div>
+                            <div class="dd">외부에서 가져온 먹이는 줄 수 없습니다</div>
+                        </div>
+                        <div class="box box-8">
+                            <div class="tit">조류 체험장</div>
+                            <p>내 손으로 직접 새들에게 먹이를 줄 수 있는 먹이체험</p>
+                            <div class="time">먹이요금 1캡슐 당 1,000원<br>(자판기 운영 / 500x2)</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="section section3">Section 3</div>
-        <div class="section section4">Section 4</div>
+    
+
+
+
+        <div class="section section3">
+    <div class="bg">
+                <div class="img-box">
+                    <div class="black"></div>
+                </div>
+            </div>
+            <div class="con">
+                <div class="title">Information</div>
+                <div class="box box1">
+                    <div class="txt">
+                        <div class="title">이용시간</div>
+                        <div class="time">
+                            <div class="1">평일  10:00-19:00<br>입장마감  18:00</div>
+                            <div class="2">주말 및 공휴일  10:00-20:00<br>입장마감  19:00</div>
+                        </div>
+                        <div class="small">※ 아쿠아리움 사정으로 이용시간이 변경될 수 있습니다.
+</div>
+                    </div>
+                </div>
+                <div class="box box2"></div>
+                <div class="box box3"></div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="section section4">
+    <div class="bg">
+                <div class="img-box">
+                    <div class="black"></div>
+                </div>
+            </div>
+            <div class="con">
+                <div class="title">Information</div>
+</div>
+    </div>
     </div>
 
 
