698bad45a2aabde2016a0b97e7ce093b7e48e8ae
Hwanghyeonjeong <hyeonjeonghwang10@gmail.com>
2020-09-24 18:48:17 +0900

    portfolio

--- public/pf/portfolio/portfolio.php
+++ public/pf/portfolio/portfolio.php
@@ -18,49 +18,76 @@ include "./head_body.php"
 ?>
 
 <div class="back">
-<div class="tpfh flex">
-    <div class="line line-1"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/tjs.png" alt=""></div>
-    <div class="line line-2"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/tjs.png" alt=""></div>
+    <div class="tpfh flex">
+        <div class="line line-1"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/tjs.png" alt=""></div>
+        <div class="line line-2"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/tjs.png" alt=""></div>
     </div>
     <div class="rkfh">
-    <div class="line line-1"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/tjstjs.png" alt=""></div>
-    <div class="line line-2"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/tjstjs.png" alt=""></div>
+        <div class="line line-1"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/tjstjs.png" alt=""></div>
+        <div class="line line-2"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/tjstjs.png" alt=""></div>
     </div>
 </div>
 
 <div class="menu-2 flex con">
-    <div class="web"><a href="./portfolio.php">Web</a></div> 
-    <div class="design"><a href="./design.php">Design</a></div> 
+    <div class="web"><a href="./portfolio.php">Web</a></div>
+    <div class="design"><a href="./design.php">Design</a></div>
 </div>
 
 <div class="center">
-<img class="computer" src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/Apple iMac.png" alt="">
-<img class="site" src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/대전아쿠아리움.PNG" alt=""></div>
+    <img class="computer" src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/Apple iMac.png" alt="">
+</div>
+</div>
+
+<div class="photo">
+    <div class="img-box"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/1.png" alt=""></div>
+    <div class="img-box"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/2.png" alt=""></div>
+    <div class="img-box"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/3.png" alt=""></div>
+    <div class="img-box"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/4.png" alt=""></div>
+    <div class="img-box"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/5.png" alt=""></div>
+    <div class="img-box"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/6.png" alt=""></div>
+    <div class="img-box"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/7.png" alt=""></div>
 </div>
 
 
 
-	  	<div class="slider-div">
-            <div>
-                <div class="sub">Creative</div>
+<div class="slider-div">
+    <div>
+        <div class="sub">Creative</div>
         <div class="title">대전아쿠아리움</div>
     </div>
-		    <div>        <div class="sub">Creative</div>
-        <div class="title">배달의 민족</div></div>
-		    <div>        <div class="sub">Creative</div>
-        <div class="title">카카오페이지</div></div>
-		    <div>        <div class="sub">Creative</div>
-        <div class="title">서브웨이</div></div>
-		    <div>        <div class="sub">Creative</div>
-        <div class="title">카카오뱅크</div></div>
-		    <div>        <div class="sub">Creative</div>
-        <div class="title">PUBG</div></div>
-		    <div>        <div class="sub">Creative</div>
-        <div class="title">경기도박물관</div></div>
-		   
-	  	</div>
+    <div>
+        <div class="sub">Creative</div>
+        <div class="title">배달의 민족</div>
     </div>
-    <div class="but">
-<div class="next"><img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FTWq2J%2FbtqEBdA2ene%2FhGRW5eH7EFHllWqfIGDFr1%2Fimg.png" alt=""></div>
-  <div class="prev"><img src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FcK32wf%2FbtqEBnQ2bSV%2FRUnYwxbL6CANymmbJNtVk0%2Fimg.png" alt=""></div>
-  </div></div>
+    <div>
+        <div class="sub">Creative</div>
+        <div class="title">카카오페이지</div>
+    </div>
+    <div>
+        <div class="sub">Creative</div>
+        <div class="title">서브웨이</div>
+    </div>
+    <div>
+        <div class="sub">Creative</div>
+        <div class="title">카카오뱅크</div>
+    </div>
+    <div>
+        <div class="sub">Creative</div>
+        <div class="title">PUBG</div>
+    </div>
+    <div>
+        <div class="sub">Creative</div>
+        <div class="title">경기도박물관</div>
+    </div>
+
+</div>
+</div>
+<div class="but">
+    <div class="next"><img
+            src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FTWq2J%2FbtqEBdA2ene%2FhGRW5eH7EFHllWqfIGDFr1%2Fimg.png"
+            alt=""></div>
+    <div class="prev"><img
+            src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FcK32wf%2FbtqEBnQ2bSV%2FRUnYwxbL6CANymmbJNtVk0%2Fimg.png"
+            alt=""></div>
+</div>
+</div>
--- public/pf/portfolio/resource/portfolio.js
+++ public/pf/portfolio/resource/portfolio.js
@@ -1,15 +1,24 @@
-$(function() {
-$('.slider-div').slick({
-    slide: 'div',		//슬라이드 되어야 할 태그 ex) div, li 
-    infinite : false, 	//무한 반복 옵션
-    slidesToShow : 2,		// 한 화면에 보여질 컨텐츠 개수
-    slidesToScroll : 1,		//스크롤 한번에 움직일 컨텐츠 개수
-    speed : 500,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-    arrows : true, 		// 옆으로 이동하는 화살표 표시 여부
-    dots : false, 		// 스크롤바 아래 점으로 페이지네이션 여부
-    prevArrow : '.prev',		// 이전 화살표 모양 설정
-    nextArrow : '.next',		// 다음 화살표 모양 설정
-    draggable : true, 	//드래그 가능 여부
-});
+function currentIndex(){
+    var $Items = $('.photo .img-box');
+    var current = parseInt($(".slider-div .slick-current").attr("data-slick-index"));
+    var $currentItem = $Items.eq(current);
+    $currentItem.addClass('active');
+}
 
+
+
+$(function() {
+    $('.slider-div').slick({
+        slide: 'div',		//슬라이드 되어야 할 태그 ex) div, li 
+        infinite : false, 	//무한 반복 옵션
+        slidesToShow : 2,		// 한 화면에 보여질 컨텐츠 개수
+        slidesToScroll : 1,		//스크롤 한번에 움직일 컨텐츠 개수
+        speed : 500,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
+        arrows : true, 		// 옆으로 이동하는 화살표 표시 여부
+        dots : false, 		// 스크롤바 아래 점으로 페이지네이션 여부
+        prevArrow : '.prev',		// 이전 화살표 모양 설정
+        nextArrow : '.next',		// 다음 화살표 모양 설정
+        draggable : true, 	//드래그 가능 여부
+        $(".slider-div").on("afterChange", currentIndex);
+    });
 })
