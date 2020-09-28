c76f9ad45ef8191f77fd2d6d66354b8d95b9db4c
Hwanghyeonjeong <hyeonjeonghwang10@gmail.com>
2020-09-28 18:48:35 +0900

    slider

--- public/pf/portfolio/about.php
+++ public/pf/portfolio/about.php
@@ -13,4 +13,41 @@ include "./head_head.php"
 include "./head_body.php"
 ?>
 <!-- 여기부터 작성하시면 됩니다. -->
+<div class="back">
 <img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/meme.png?" alt="">
+</div>
+<div class="profile con">
+    <div class="title">
+        <img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/post.png?" alt="">
+        <div class="text">profile</div>
+    </div>
+    <div class="name flex">
+        <div class="kr">황현정</div>
+        <div class="en">Hwang Hyeon jeong</div>
+    </div>
+    <div class="info">2000.01.25<br>
+대전광역시 서구 둔산동<br>
+010 3370 5680<br>
+E-mail : mn06150@naver.com<br>
+wiki : to2.kr/bk3<br>
+Blog : https://hyeonjeonghwang.tistory.com/</div>
+</div>
+
+<div class="skills">
+<div class="title">
+        <img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/post.png?" alt="">
+        <div class="text">Skills</div>
+    </div>
+    <div class="photo">
+        <img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/html.png" alt="">
+        <img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/css.png" alt="">
+        <img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/js.png" alt="">
+        <img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/ps.png" alt="">
+        <img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/ai.png" alt="">
+    </div>
+</div>
+
+
+<div class="chart">
+    <canvas id="line-chart" width="450" height="100"></canvas>
+</div>
--- public/pf/portfolio/resource/about.js
+++ public/pf/portfolio/resource/about.js
@@ -1,49 +1,49 @@
 $(function() {
-var marksCanvas = document.getElementById("line-chart");
-
-Chart.defaults.global.defaultFontFamily = "Lato";
-Chart.defaults.global.defaultFontSize = false;
-Chart.defaults.scale.gridLines.display = false;
-
-var radarChart = new Chart(marksCanvas, {
-    type: "line",
-    data: {
-        labels: ["ex01", "ex02", "ex03", "ex04", "ex05"],
-        datasets: [
-            {
-                label: "none",
-                borderColor: "#000000",
-                pointBorderColor: "#000000",
-                pointBackgroundColor: "#000000",
-                pointBorderWidth: 1,
-                pointHoverRadius: 4,
-                pointRadius: 4,
-                lineTension: 0,
-                backgroundColor: "transparent",
-                fill: false,
-                borderWidth: 2,
-                data: [90, 90, 65, 80, 85]
-            }
-        ]
-    },
-    options: {
-        responsive: false,
-        maintainAspectRatio: true,
-        scale: {
-            ticks: {
-                display: true,
-                beginAtZero: true,
-                min: 0,
-                max: 100,
-                stepSize: 20
+    var marksCanvas = document.getElementById("line-chart");
+    
+    Chart.defaults.global.defaultFontFamily = "Lato";
+    Chart.defaults.global.defaultFontSize = false;
+    Chart.defaults.scale.gridLines.display = false;
+    
+    var radarChart = new Chart(marksCanvas, {
+        type: "line",
+        data: {
+            labels: ["ex01", "ex02", "ex03", "ex04", "ex05"],
+            datasets: [
+                {
+                    label: "none",
+                    borderColor: "#000000",
+                    pointBorderColor: "#000000",
+                    pointBackgroundColor: "#000000",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 4,
+                    pointRadius: 4,
+                    lineTension: 0,
+                    backgroundColor: "transparent",
+                    fill: false,
+                    borderWidth: 2,
+                    data: [90, 90, 65, 80, 85]
+                }
+            ]
+        },
+        options: {
+            responsive: false,
+            maintainAspectRatio: true,
+            scale: {
+                ticks: {
+                    display: true,
+                    beginAtZero: true,
+                    min: 0,
+                    max: 100,
+                    stepSize: 20
+                },
+                pointLabels: {
+                    fontSize: 18
+                }
             },
-            pointLabels: {
-                fontSize: 18
-            }
-        },
-        legend: {
-            display: false
-        },
-    }
-});
-})
+            legend: {
+                display: false
+            },
+        }
+    });
+    })
