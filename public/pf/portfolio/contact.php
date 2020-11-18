776a9fcaf00d1698731430bb8afc89dd201cc04b
hwanghyeonjeong <hyeonjeonghwang10@gmail.com>
2020-11-18 23:35:49 +0900

    re

--- public/pf/portfolio/contact.php
+++ public/pf/portfolio/contact.php
@@ -9,7 +9,7 @@ include "./head_head.php"
 include "./head_body.php"
 ?>
 
-<div class="title con"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/contact2.png?dummy" alt=""></div>
+<div class="title con"><img src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/contact4.png" alt=""></div>
 
 <div class="cover">
 <img class="tjs"src="https://hwanghyeonjeong.github.io/img1/pf/portfolio/tjstjs.png?dummy" alt="">
